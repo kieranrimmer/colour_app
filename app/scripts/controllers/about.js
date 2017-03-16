0f619d63d97a2f192a6ee53bbcab5aa963a99b90
Kieran Rimmer <kieran.rimmer@servian.com>
2017-03-16 23:42:38 +1100

    basic endpoint checking implemented

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,35 @@
  * Controller of the colourappApp
  */
 angular.module('colourappApp')
-  .controller('AboutCtrl', function ($scope) {
+  .controller('AboutCtrl', function ($scope, $http, $q) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.response = {};
+    $scope.error = {};
+
+    function callGoogle() {
+            $http.get('http://www.google.com', {}).then(
+                function success(response) {
+                    $scope.response = response;
+                },
+                function failure(error) {
+                    $scope.error = error;
+                }
+            )
+        };
+
+    $scope.getGoogleCheck = function() {
+            $http.get('http://www.google.com', {}).then(
+                function success(response) {
+                    $scope.response = response;
+                },
+                function failure(error) {
+                    $scope.error = error;
+                }
+            )
+    }
   });
