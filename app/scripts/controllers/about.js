056b7054455bace914ecddee85ab32a2c4ed42b8
Kieran Rimmer <kieran.rimmer@servian.com>
2017-03-17 08:09:42 +1100

    skeleton improving

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,35 +8,86 @@
  * Controller of the colourappApp
  */
 angular.module('colourappApp')
-  .controller('AboutCtrl', function ($scope, $http, $q) {
+  .controller('AboutCtrl', function ($scope, $http, $q, asyncService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-    $scope.response = {};
+    $scope.response = {body: null};
     $scope.error = {};
 
-    function callGoogle() {
-            $http.get('http://www.google.com', {}).then(
-                function success(response) {
-                    $scope.response = response;
-                },
-                function failure(error) {
-                    $scope.error = error;
-                }
-            )
-        };
+
 
     $scope.getGoogleCheck = function() {
-            $http.get('http://www.google.com', {}).then(
+            asyncService.getGoogleCheckRecursive()
+                .then(
                 function success(response) {
+                    console.log('success triggered');
                     $scope.response = response;
                 },
                 function failure(error) {
+                    console.log('failure triggered');
                     $scope.error = error;
                 }
             )
     }
   });
+
+angular.module('colourappApp')
+    .factory('asyncService', function ($http, $q, $log) {
+        var counter = 0;
+
+        function getGoogleCheckRecursive() {
+            var deferred = $q.defer();
+            $http.get('http://www.google.com', {}).then(
+                function success(response) {
+                    deferred.resolve(response);
+                    return deferred.promise;
+                },
+                function failure(error) {
+                    console.log('counter = ' + counter + ', error = ' + JSON.stringify(error,null,2));
+                    if(counter < 2) {
+                        counter++;
+                        return getGoogleCheck();
+                    }
+                    else {
+                        console.log('counter = ' + counter + ', rejecting error = ' + JSON.stringify(error,null,2));
+                        deferred.reject(error);
+                        return deferred.promise;
+                    }
+                }
+            );
+            return deferred.promise;
+        }
+
+        function getGoogleCheck(bodyToPopulate) {
+            var deferred = $q.defer();
+            $http.get('http://www.google.com', {}).then(
+                function success(response) {
+                    bodyToPopulate = response;
+                    deferred.resolve(response);
+                },
+                function failure(error) {
+                    bodyToPopulate = error;
+                    console.log('counter = ' + counter + ', error = ' + JSON.stringify(error,null,2));
+                    if(counter < 2) {
+                        counter++;
+                        return getGoogleCheck();
+                    }
+                    else {
+                        console.log('counter = ' + counter + ', rejecting error = ' + JSON.stringify(error,null,2));
+                        deferred.reject(error);
+                    }
+                }
+            );
+            return deferred.promise;
+        }
+
+
+        return {
+            getGoogleCheckRecursive: getGoogleCheckRecursive,
+            getGoogleCheck: getGoogleCheck
+        };
+    });
